c10b18c6a95cb11a1da77bc46fabe690162bb328
haarvs <harvey.yang@outlook.com>
2015-01-27 04:01:28 +0000

    trying to find the correct permutation of nickname

--- server.js
+++ server.js
@@ -13,4 +13,8 @@ app.get('/brave-newt', function(request, response){
     response.send('Hello MGT-656! This is brave newt!');
 });
 
+app.get('/brave newt', function(request, response){
+    response.send('Hello MGT-656! This is brave newt!');
+});
+
 app.listen(process.env.PORT || 4000);
