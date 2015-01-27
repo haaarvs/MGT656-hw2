f2abf565ceb900d2ca33c05f278c321343619c39
haarvs <harvey.yang@outlook.com>
2015-01-27 00:14:56 +0000

    added nickname path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+app.get('/brave%20newt', function(request, response){
+    response.send('Hello MGT-656! This is brave newt!');
 });
 
 app.listen(process.env.PORT || 4000);
