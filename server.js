c7e3b89154880471bc3572fd5786137399075fb5
haarvs <harvey.yang@outlook.com>
2015-01-27 04:05:04 +0000

    finding right path

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ app.get('/brave-newt', function(request, response){
     response.send('Hello MGT-656! This is brave newt!');
 });
 
+app.get('/brave+newt', function(request, response){
+    response.send('Hello MGT-656! This is brave newt!');
+});
+
 app.get('/brave newt', function(request, response){
     response.send('Hello MGT-656! This is brave newt!');
 });
+
+app.get('/brave%20newt', function(request, response){
+    response.send('Hello MGT-656! This is brave newt!');
+});
+
 
 app.listen(process.env.PORT || 4000);
